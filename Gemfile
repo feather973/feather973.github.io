21447ab5b70ccf89e2e4a72ac88392618a92d8a4
feather973 <feather973@daum.net>
2021-08-16 00:47:11 -0700

    Mon 16 Aug 2021 12:47:11 AM PDT

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ gemspec
 
 group :jekyll_plugins do
 	gem "jekyll-drawio"
+	gem "kramdown-parser-gfm"
 end
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "Table",
+        "excerpt":"hello my table 안녕하세요, 첫번째 포스트입니다. #include &lt;stdio.h&gt; #include &lt;limits.h&gt; // glibc use 2147483647 // kernfs-internal.h use below //#define INT_MAX ((int)(~0U &gt;&gt; 1)) #define INT_MIN (-INT_MAX - 1) #define KN_DEACTIVATED_BIAS (INT_MIN + 1) #define KN_DEACTIVATED_BIAS2 (INT_MIN) int main() { printf(&quot;0x%x\\n&quot; ,KN_DEACTIVATED_BIAS); printf(&quot;0x%x\\n&quot; ,KN_DEACTIVATED_BIAS2); return 0; } drawio 예시입니다. {% drawio...","categories": [],
+        "tags": [],
+        "url": "/table/",
+        "teaser": null
+      }]
