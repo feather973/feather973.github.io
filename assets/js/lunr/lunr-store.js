5846638cb551b204a86cf67a6b78af9d5a17e737
feather973 <feather973@daum.net>
2021-08-16 03:05:45 -0700

    new posts3

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Table",
-        "excerpt":"hello my table   안녕하세요, 첫번째 포스트입니다.   #include &lt;stdio.h&gt; #include &lt;limits.h&gt;  // glibc use 2147483647 // kernfs-internal.h use below //#define INT_MAX ((int)(~0U &gt;&gt; 1))  #define INT_MIN  (-INT_MAX - 1) #define KN_DEACTIVATED_BIAS     (INT_MIN + 1) #define KN_DEACTIVATED_BIAS2     (INT_MIN)  int main() {     printf(&quot;0x%x\\n&quot; ,KN_DEACTIVATED_BIAS);     printf(&quot;0x%x\\n&quot; ,KN_DEACTIVATED_BIAS2);     return 0; }   drawio 예시입니다.     1111  ","categories": [],
+        "excerpt":"hello my table 안녕하세요, 첫번째 포스트입니다. #include &lt;stdio.h&gt; #include &lt;limits.h&gt; // glibc use 2147483647 // kernfs-internal.h use below //#define INT_MAX ((int)(~0U &gt;&gt; 1)) #define INT_MIN (-INT_MAX - 1) #define KN_DEACTIVATED_BIAS (INT_MIN + 1) #define KN_DEACTIVATED_BIAS2 (INT_MIN) int main() { printf(&quot;0x%x\\n&quot; ,KN_DEACTIVATED_BIAS); printf(&quot;0x%x\\n&quot; ,KN_DEACTIVATED_BIAS2); return 0; } drawio 예시입니다. 1111 image...","categories": [],
         "tags": [],
         "url": "/table/",
         "teaser": null
