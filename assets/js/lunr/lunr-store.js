b312d089964131b8a8551e12bd633345bb52e0a0
feather973 <feather973@daum.net>
2021-08-24 16:09:57 -0700

    new posts

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,14 +5,14 @@ var store = [{
         "url": "/jekyll/update/welcome-to-jekyll/",
         "teaser": null
       },{
-        "title": "Table",
-        "excerpt":"hello my table 안녕하세요, 첫번째 포스트입니다. 하하하 #include &lt;stdio.h&gt; #include &lt;limits.h&gt; // glibc use 2147483647 // kernfs-internal.h use below //#define INT_MAX ((int)(~0U &gt;&gt; 1)) #define INT_MIN (-INT_MAX - 1) #define KN_DEACTIVATED_BIAS (INT_MIN + 1) #define KN_DEACTIVATED_BIAS2 (INT_MIN) int main() { printf(&quot;0x%x\\n&quot; ,KN_DEACTIVATED_BIAS); printf(&quot;0x%x\\n&quot; ,KN_DEACTIVATED_BIAS2); return 0; } drawio 예시입니다. 1111...","categories": [],
+        "title": "Example",
+        "excerpt":"플러그인 사용예시를 소개하는 게시물입니다. gist 에시입니다. #include &lt;stdio.h&gt; #include &lt;limits.h&gt; // glibc use 2147483647 // kernfs-internal.h use below //#define INT_MAX ((int)(~0U &gt;&gt; 1)) #define INT_MIN (-INT_MAX - 1) #define KN_DEACTIVATED_BIAS (INT_MIN + 1) #define KN_DEACTIVATED_BIAS2 (INT_MIN) int main() { printf(&quot;0x%x\\n&quot; ,KN_DEACTIVATED_BIAS); printf(&quot;0x%x\\n&quot; ,KN_DEACTIVATED_BIAS2); return 0; } drawio 예시입니다. image 예시입니다....","categories": [],
         "tags": [],
-        "url": "/table/",
+        "url": "/example/",
         "teaser": null
       },{
         "title": "Timekeeper 1",
-        "excerpt":"Timekeeper-1 목차 1) PTP 프로토콜 예시 2) array_index_mask_nospec 에서 ~ 쓰는 이유 3) timekeeper xtime 4) shadow timekeeping 1) PTP 프로토콜 예시 https://endruntechnologies.com/pdf/PTP-1588.pdf 포인트는 SLAVE CLOCK 을 MASTER CLOCK 에게 맞추는 것입니다. 2) array_index_mask_nospec 에서 ~ 쓰는 이유 https://dojang.io/mod/page/view.php?id=183 포인트는 정상범위일땐 음수비트를 '산술시프트'하여 0xffffffff 마스크를 비정상범위일땐 양수비트를 산술시프트하여 0x0 마스크를...","categories": [],
+        "excerpt":"PTP 프로토콜과 xtime 을 소개하는 게시물입니다. 목차 1) PTP 프로토콜 예시 2) array_index_mask_nospec 에서 ~ 의 의미 3) timekeeper xtime 4) (next) shadow timekeeping 1) PTP 프로토콜 예시 https://endruntechnologies.com/pdf/PTP-1588.pdf 프로토콜 동작의 포인트는 SLAVE CLOCK 을 MASTER CLOCK 에게 맞추는 것입니다. 2) array_index_mask_nospec 에서 ~ 의 의미 https://dojang.io/mod/page/view.php?id=183 정상범위일땐 음수비트를 산술시프트하여...","categories": [],
         "tags": [],
         "url": "/timekeeper-1/",
         "teaser": null
